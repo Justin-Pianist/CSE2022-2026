0101af61d789a5683d86db38de3bed53a17a04a7
kishorekrish007 <149303061+kishorekrish007@users.noreply.github.com>
2023-10-29 17:03:30 +0530

    Update chek.java

--- chek.java
+++ chek.java
@@ -2,4 +2,4 @@ class chek{
     public static void main(String args[]){
         System.out.println("WHat is your name ?");
         System.out.println("Naan thaan da Leo!!") ;
-}}
+        System.out.println("Justin loves passumaadu!!!") ;
